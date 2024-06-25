646ab1bd280c6eaf9ecc3ca94dfad0c70f6575ec
Aaron Klump <sourcecode@intheloftstudios.com>
2024-06-24 17:28:29 -0700

    Add more power

--- build_url.fn.php
+++ build_url.fn.php
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 /**
- * Create an URL from the array returned by parse_url().
+ * Create an URL from components such as returned by parse_url().
  *
  * @param array $components
  *
@@ -11,17 +11,35 @@
  * @throws \InvalidArgumentException If the components cannot be used to
  * construct an URL.
  * @see \parse_url()
- *
  */
 function build_url(array $components): string {
-  $scheme = $components['scheme'] ?? 'https';
+  $default_scheme = 'https';
+  $scheme = $components['scheme'] ?? $default_scheme;
   $host = $components['host'] ?? '';
+  if(!empty($components['port'])) {
+    $host .= ':'.$components['port'];
+  }
   $path = $components['path'] ?? '';
-
-  // Credentialed URL
-  if (!empty($components['user']) && !empty($components['pass'])) {
-    return sprintf('%s://%s:%s@%s%s', $scheme, $components['user'], $components['pass'], $host, $path);
+  if ($path) {
+    $path = '/' . ltrim($path, '/');
   }
 
-  return sprintf('%s://%s%s', $scheme, $host, $path);
+  // Credentialed URL
+  if (!empty($components['user'])) {
+    $pass = $components['pass'] ?? '';
+    $pass = $pass ?: 'PASSWORD';
+
+    return sprintf('%s://%s:%s@%s%s', $scheme, $components['user'], $pass, $host, $path);
+  }
+
+  $url = sprintf('%s://%s%s', $scheme, $host, $path);
+  if (!empty($components['query'])) {
+    $url .= '?' . $components['query'];
+  }
+
+  if (empty($components['scheme']) && "$default_scheme://" === $url) {
+    throw new InvalidArgumentException('$components cannot be understood so as to build URL.');
+  }
+
+  return $url;
 }
--- tests_unit/BuildURLTest.php
+++ tests_unit/BuildURLTest.php
@@ -11,12 +11,10 @@ class BuildURLTest extends TestCase {
 
   public function dataForProvider() {
     $tests = [];
-    $tests[] = [
-      'https://stackoverflow.com/questions/37906058/combine-url-parts-in-php',
-    ];
-    $tests[] = [
-      'mysql://drupal8:drupal8@database--develop/drupal8',
-    ];
+    $tests[] = ['https://stackoverflow.com/questions/37906058/combine-url-parts-in-php'];
+    $tests[] = ['mysql://drupal8:drupal8@database--develop/drupal8'];
+    $tests[] = ['https://github.com/aklump/build-url/security?foo=bar'];
+    $tests[] = ['mysql://drupal8:rock$ol1D@localhost:3306/drupal8'];
 
     return $tests;
   }
@@ -24,9 +22,40 @@ class BuildURLTest extends TestCase {
   /**
    * @dataProvider dataForProvider
    */
-  public function testInvoke(string $url) {
+  public function testParseThenBuildAsExpected(string $url) {
     $parts = parse_url($url);
     $this->assertSame($url, build_url($parts));
+  }
+
+  public function dataFortestBuildFromArraysProvider() {
+    $tests = [];
+    $tests[] = [
+      ['host' => 'lorem.com'],
+      'https://lorem.com',
+    ];
+    $tests[] = [
+      ['host' => 'lorem.com', 'path' => 'page1.html'],
+      'https://lorem.com/page1.html',
+    ];
+    $tests[] = [
+      ['host' => 'lorem.com', 'user' => 'admin'],
+      'https://admin:PASSWORD@lorem.com',
+    ];
+
+
+    return $tests;
+  }
+
+  /**
+   * @dataProvider dataFortestBuildFromArraysProvider
+   */
+  public function testBuildFromArrays(array $components, string $expected) {
+    $this->assertSame($expected, build_url($components));
+  }
+
+  public function testEmptyArrayThrows() {
+    $this->expectException(\InvalidArgumentException::class);
+    build_url([]);
   }
 
 }
